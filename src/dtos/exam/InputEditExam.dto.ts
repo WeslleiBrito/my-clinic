d6e589b359f5c587a6014fdb7762417abfed3df2
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2023-11-20 10:19:35 -0300

    Correção da obrigatoriedade de enviar o nome do exame

--- src/dtos/exam/InputEditExam.dto.ts
+++ src/dtos/exam/InputEditExam.dto.ts
@@ -15,7 +15,7 @@ export interface OutputEditExamDTO {
 export const InputEditExamSchema = z.object(
     {
         id: z.string({required_error: "O id do exame deve ser informado.", invalid_type_error: "Espera-se que o id venha como uma string."}),
-        name: z.string({invalid_type_error: "O nome deve ser do tipo string."}).min(3, {message: "O nome do exame precisa terno mínimo 3 caracters."}),
+        name: z.string({invalid_type_error: "O nome deve ser do tipo string."}).min(3, {message: "O nome do exame precisa terno mínimo 3 caracters."}).optional(),
         price: z.number({invalid_type_error: "O preço deve ser do tipo number."}).nonnegative({message: "O preço não pode ser menor que zero."}).optional()
     }
 ).transform(data => data as InputEditExamDTO)
