0dc2b0d1fa96e3f563337ae2be34563218982855
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2024-02-08 10:55:31 -0300

    melhora na performace da geração do pdf

--- src/services/Print.ts
+++ src/services/Print.ts
@@ -50,37 +50,45 @@ export class Print {
             });
         }
 
-        const risksOccupational = dataForm.OccupationalHazards.map((risk) => {
-            return `${risk.name}`
+        let risksHtml = ""
+
+        dataForm.OccupationalHazards.forEach((risk) => {
+            risksHtml += `<li>${risk.name}</li>`
         })
 
-        const typeExam = typeExamAso.map((type) => {
-            return `
+        let typeExamHtml = ""
+
+        typeExamAso.forEach((type) => {
+            typeExamHtml += `
+                <li>
                     <input type="radio" id=${type.id} name=${type.name} value=${type.id} ${type.selected ? "checked" : ""}/>
-                    <lable for=${type.id}>${type.name}</lable>                
+                    <lable for=${type.id}>${type.name}</lable>
+                </li>                
             `
         })
 
-        const exams = listExamsAll.map((exam, index) => {
-            return `
-                    <tr id=${exam.id}>
-                        <td>${index + 1}. ${exam.name}</td>
-                        <td>${exam.date}</td>
-                    </tr>
-                `
+        let examsHtml = ""
+
+        listExamsAll.forEach((exam, index) => {
+            examsHtml += `
+            <tr id=${exam.id}>
+                <td>${index + 1}. ${exam.name}</td>
+                <td>${exam.date}</td>
+            </tr>
+        `
         })
 
         try {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
             const data = {
-                typeExam,
+                typeExam: typeExamHtml,
                 nameCompany: dataForm.nameCompany,
                 cnpj: dataForm.cnpj,
                 namePatient: dataForm.namePatient,
                 rg: dataForm.rg,
-                riscks: risksOccupational,
-                exams,
+                riscks: risksHtml,
+                exams: examsHtml,
                 status: dataForm.status ? "APTO" : "INAPTO",
                 functionPatient: dataForm.functionPatient.toUpperCase(),
                 comments: dataForm.comments ? dataForm.comments : "",
@@ -88,11 +96,11 @@ export class Print {
             }
 
             const pathHTML = path.join(__dirname, '../templates/form/form.html')
-            const newHtml = await renderHtml(pathHTML, data)
+            const htmlPage = await renderHtml(pathHTML, data)
 
             // Definir o conteúdo da página com o HTML renderizado
 
-            await page.setContent(newHtml);
+            await page.setContent(htmlPage);
 
             // Gerar o PDF
             await page.pdf({ path: 'output.pdf', format: 'A4' });
@@ -101,79 +109,5 @@ export class Print {
         } catch (error) {
             console.log(error)
         }
-
-        /* async function generatePDF() {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-
-            // Seu HTML com estilos inline ou referências a arquivos CSS
-            const htmlContent = `
-                <html>
-                    <head>
-                        <title>Exemplo de PDF</title>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                                background-color: #f0f0f0;
-                            }
-                            h1 {
-                                color: blue;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <ul>
-                            ${typeExam}
-                        </ul>
-                        <p>ATESTO QUE O(A) ${dataForm.namePatient}, RG ${dataForm.rg} submeteu-se a avliação de saúde, conforme regulamenta a Portaria
-                            Ministerial Nº 27 e 29 dezembro de 1994, NR-7, PCMSO.
-                        </p>
-                        <p>Empresa</p>
-                        <ul>
-                            <li>Nome: ${dataForm.nameCompany}</li>
-                            <li>CNPJ: ${dataForm.cnpj}</li>
-                        </ul>
-                        <p>Riscos ocupacionais</p>
-                        <ul>
-                            ${risksOccupational}
-                        </ul>
-                        <table border="1">
-                            <caption>Exames Realizado</caption>
-                            <thead>
-                                <tr>
-                                    <th>Tipo</th>
-                                    <th>Data</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                ${exams}
-                            </tbody>
-                        </table>
-                        <P>Foi considerado ${dataForm.status ? "APTO" : "INAPTO"} à desempenhar a função de ${dataForm.functionPatient.toUpperCase()}.</P>
-                    </body>
-                </html>
-            `;
-            const data = {
-                typeExam: typeExamAso
-            }
-
-            const pathHTML = path.join(__dirname, '../templates/form/form.html')
-            const newHtml = await renderHtml(pathHTML, data)
-
-            // Definir o conteúdo da página com o HTML renderizado
-
-            await page.setContent(newHtml);
-
-            // Gerar o PDF
-            await page.pdf({ path: 'output.pdf', format: 'A4' });
-
-            await browser.close();
-        }
-
-        generatePDF().then(() => {
-            console.log('PDF gerado com sucesso.');
-        }).catch((err) => {
-            console.error('Erro ao gerar PDF:', err);
-        }); */
     }
 }
